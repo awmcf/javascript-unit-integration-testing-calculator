9f5aeaf5cf38c483322403883e70a01491eb2f19
awmcf <aaronmcfaull@gmail.com>
2019-03-24 13:46:09 +0000

    unit tests pass

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -9,6 +9,31 @@ describe('calculator', function () {
   // write unit tests here in the form of "it should do something..."
   it('it has a sample test', function(){
     assert.equal(true, true)
-  })
+  });
+
+  it('it should add numbers', function(){
+    calculator.previousTotal = 4;
+    calculator.add(1);
+    assert.equal(calculator.runningTotal, 5)
+  });
+
+  it('it should subtract numbers', function(){
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    assert.equal(calculator.runningTotal, 3)
+  });
+
+  it('it should multiply numbers', function(){
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.equal(calculator.runningTotal, 15)
+  });
+
+  it('it should divide numbers', function(){
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    assert.equal(calculator.runningTotal, 3)
+  });
+
 
 });
